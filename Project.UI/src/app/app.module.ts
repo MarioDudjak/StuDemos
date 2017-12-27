39638b4bff22de6ebd233127aad906d2559a83bb
kjavorek <kjavorek@etfos.hr>
2017-12-27 13:32:43 +0100

    Merge conflicts

--- Project.UI/src/app/app.module.ts
+++ Project.UI/src/app/app.module.ts
@@ -3,43 +3,28 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-<<<<<<< HEAD
-import {StudentModule} from './student/student.module';
-import {SharedModule} from './shared/shared.module';
+import { SharedModule } from './shared/shared.module';
+import { LayoutsModule } from './layouts';
+import { RoutesModule } from './routes';
+
 import { HomeComponent } from './home/home.component';
+import { ComponentsModule } from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
-=======
-import { SharedModule } from './shared/shared.module';
-import { MaterializeModule } from 'angular2-materialize';
-import { FilterPipe } from '../pipes';
-import {LayoutsModule} from './layouts';
-import {RoutesModule} from './routes';
-import {ComponentsModule} from './components';
-@NgModule({
-  declarations: [
-    AppComponent,
-    FilterPipe,
->>>>>>> 6226376a5b34e351f2c8f1d43e82f660028586b8
   ],
   imports: [
     AppRoutingModule,
     SharedModule,
-    MaterializeModule,
     LayoutsModule,
     RoutesModule,
     ComponentsModule
   ],
   providers: [],
-<<<<<<< HEAD
-  bootstrap: [AppComponent, HomeComponent]
-=======
   bootstrap: [
-    AppComponent,
+    AppComponent
   ]
->>>>>>> 6226376a5b34e351f2c8f1d43e82f660028586b8
 })
 export class AppModule { }
