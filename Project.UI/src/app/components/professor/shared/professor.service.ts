954b6994e27999dddbd2a0192d92c7096443eb0c
kjavorek <kjavorek@etfos.hr>
2018-01-24 00:16:16 +0100

    List of demonstrations

--- Project.UI/src/app/components/professor/shared/professor.service.ts
+++ Project.UI/src/app/components/professor/shared/professor.service.ts
@@ -6,5 +6,11 @@ import { HttpService } from '../../../shared/';
 export class ProfessorService {
 	
     constructor(private httpService: HttpService) { }
+
+    //Get demonstrators and courses
+    public async getDemoCourses() : Promise<Object> {
+        return [{"courseName":"Baze podataka","courseId":"id kolegija","students":["Marija","Lucija","Ana"]},
+        {"courseName":"Programiranje I","courseId":"id kolegija","students":["Lovro","Duje","Roko"]}];
+    }
 	
 }
