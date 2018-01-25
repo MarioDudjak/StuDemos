ba7c2cb0cab4e6793cd055a51739d66753d18d35
matej94 <matej.tomic123@gmail.com>
2018-01-25 20:09:04 +0100

    Homepage updated

--- Project.UI/src/app/layouts/master.layout.ts
+++ Project.UI/src/app/layouts/master.layout.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { LoginService } from '../shared';
 
 @Component({
     selector: 'master-layout',
     templateUrl: 'master.layout.html',
-    styleUrls: ['./master.layout.css',]    
+    styleUrls: ['./master.layout.css',]
+        
 })
-export class MasterLayout { }
+
+export class MasterLayout implements OnInit {
+     
+        isLogged:boolean = false;
+        constructor(private loginService:LoginService) { }
+    ngOnInit(){
+        if(localStorage.getItem('userId')){
+            this.isLogged = true;
+        }
+    
+    }
+}
